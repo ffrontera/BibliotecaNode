6841992f100d15ff5314a94e121d34a6ae1e80d1
Verónica <molinariveronica478@gmail.com>
2024-05-07 21:34:45 -0300

    Recomendaciones listo

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,22 +1,22 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      480: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-  });
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
