333db12e913f8d956c797df9b91e23f3246f808c
ffrontera <ffrontera@live.com>
2024-05-14 17:37:33 -0300

    se agrego validacion a formulario de contacto

---
+++ js/validaciones.js
@@ -0,0 +1,32 @@
+const formContacto = document.querySelector(".formulario_contacto");
+
+formContacto.addEventListener("submit" , (event) => {
+    
+    const inputText = document.getElementsByClassName("item_formulario_contacto");
+
+    for (let i = 0; i < inputText.length; i++) {
+        
+        if (inputText[i].value.trim().length < 3 ) {
+            alert(`Debe completar el campo "${inputText[i].getAttribute("id").toLocaleUpperCase}"`);
+            event.preventDefault();
+            return;
+        }        
+    }
+    
+    if(!document.querySelector('input[name="condicion"]:checked')){
+        event.preventDefault();
+        alert("Debe seleccionar si esta asociado o no");
+        return;
+    }
+
+    const acepto = document.getElementById("terminos");
+
+    if (!acepto.checked){
+        event.preventDefault();
+        alert("Debes aceptar los términos y condiciones")
+        return;
+    }
+
+    alert("formulario enviado");
+    
+});
