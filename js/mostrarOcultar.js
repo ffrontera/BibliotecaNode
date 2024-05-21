cb6d6a1e364acd53bb6400f5f1b7b62a14c8b5bc
ffrontera <ffrontera@live.com>
2024-05-21 11:40:15 -0300

    se agrego el formulario de ingreso a separte.html

--- js/mostrarOcultar.js
+++ js/mostrarOcultar.js
@@ -1,5 +1,7 @@
 const iniciarSesion = document.getElementById("toggleIniciosesion");
 const registro = document.getElementById("toggleRegistro");
+const botonIngresar = document.getElementById("botonIngresar");
+const botonRegistro = document.getElementById("botonRegistro");
 const contenedorRegistro = document.getElementById("contenedorRegistro");
 const contenedorInicio = document.getElementById("contenedorInicioSesion");
 
@@ -7,7 +9,15 @@ iniciarSesion.addEventListener("click", () => {
     contenedorInicio.style.display = "flex";
     contenedorRegistro.style.display = "none";
 });
+botonIngresar.addEventListener("click", () => {
+    contenedorInicio.style.display = "flex";
+    contenedorRegistro.style.display = "none";
+});
 registro.addEventListener("click", () => {
+    contenedorInicio.style.display = "none";
+    contenedorRegistro.style.display = "flex";
+});
+botonRegistro.addEventListener("click", () => {
     contenedorInicio.style.display = "none";
     contenedorRegistro.style.display = "flex";
 });
